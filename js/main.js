3ddd19e69d9c8bfbc02144664c10852880896ba5
tritone11 <gioele@officinetesla.it>
2016-02-08 22:03:26 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
     var done=0;  
 $(window).scroll(function () {
            var fromTop = $(window).scrollTop();console.log('done is '+done)
-           var scrollPercentage = 100 * $(window).scrollTop() / ($('#vetrina').height() - $(window).height());
+           var scrollPercentage = 100 * $(window).scrollTop() / ($('#circlecontainer').height() - $(window).height());
            console.log(scrollPercentage);
-           if (scrollPercentage <= -234.2) {
+           if (scrollPercentage <= -71.3) {
                document.getElementById('blackheader').style.display = "block";
                if(done==0){document.getElementById('header').setAttribute('style', 'opacity:0;margin-top:-30px')}
-               if (scrollPercentage <= -238.2) {done=1
+               if (scrollPercentage <= -72.3) {done=1
                    if(done==1){ document.getElementById('header').setAttribute('style', 'opacity:1;margin-top:23px;background:transparent;');}
                    document.getElementById('header').style.position = "fixed";
                } else {done=0;
