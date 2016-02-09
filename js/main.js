ff5886d68a88b6b8e9875a2a0afbffd781f42197
tritone11 <gioele@officinetesla.it>
2016-02-09 02:08:00 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,20 +3,22 @@ $(window).scroll(function () {
            var fromTop = $(window).scrollTop();console.log('done is '+done)
            var scrollPercentage = 100 * $(window).scrollTop() / ($('#circlecontainer').height() - $(window).height());
            console.log(scrollPercentage);
+    if(scrollPercentage<=-271){$('#footer').addClass("show");}else{$('#footer').removeClass("show");}
            if (scrollPercentage <= -71.3) {
-               document.getElementById('blackheader').style.display = "block";
+               document.getElementById('blackheader').setAttribute('style','height:87px;border-bottom:5vw solid transparent;border-left:100vw solid #1a1a1a');
                if(done==0){document.getElementById('header').setAttribute('style', 'opacity:0;margin-top:-30px')}
                if (scrollPercentage <= -72.3) {done=1
-                   if(done==1){ document.getElementById('header').setAttribute('style', 'opacity:1;margin-top:23px;background:transparent;');}
+                   if(done==1){ document.getElementById('header').setAttribute('style', 'opacity:1;margin-top:23px;background:transparent;    transform: translateX(-195px);');}
                    document.getElementById('header').style.position = "fixed";
                } else {done=0;
                    document.getElementById('header').style.position = "absolute";
                }
            } else {
               document.getElementById('header').setAttribute('style', '');
-               document.getElementById('blackheader').style.display = "none";
+               document.getElementById('blackheader').setAttribute('style','height:0px;border-bottom:0vw solid transparent;border-left:100vw solid #1a1a1a');
            }
-           $("#circlecontainer").css('margin', '-' + (fromTop / 3) + 'px 0px 0px 0px');
+           $("#circlecontainer").css('margin', '-' + (fromTop/3) + 'px 0px 0px 0px');
+    $("#infographic").css('margin',   (fromTop / 2) + 'px 0px 0px 0px');
 
        });
 window.number=0;
